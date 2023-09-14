ebf5f21aac447d55cd61b66a2c0492f592e91e42
FerHdzJr <145053493+FerHdzJr@users.noreply.github.com>
2023-09-14 12:37:17 -0600

    Create DiscountCalculator.py

---
+++ DiscountCalculator.py
@@ -0,0 +1,4 @@
+print("---Welcome to Discount Calculator---")
+n=float(input("Which is the price of the product/service?\n"))
+d=float(input("Which is the discount percentage?\n"))
+print("The final price is: $"+str(n-n*d/100))
