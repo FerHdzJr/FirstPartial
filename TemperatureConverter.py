20a393661f7d28a07c0b05de22f53c3e1dbc83c9
FerHdzJr <145053493+FerHdzJr@users.noreply.github.com>
2023-09-14 12:38:06 -0600

    Create TemperatureConverter.py

---
+++ TemperatureConverter.py
@@ -0,0 +1,4 @@
+print("---Hello, welcome to Temperature converter---")
+n=float(input("Introduce the temperature in Celsius\n"))
+print("The temperature in Fahrenheit is: "+str(32+(9*n/5))+"°F")
+print("The temperature in Kevin is: "+str(273.15+n)+"°K")
