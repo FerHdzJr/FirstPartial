dcf04c3a6264311dffc5b0e9d93c519103ea2f7c
FerHdzJr <145053493+FerHdzJr@users.noreply.github.com>
2023-10-05 12:51:09 -0600

    Create round1.py

---
+++ round1.py
@@ -0,0 +1,13 @@
+def mult(a, b):
+    return a*b
+
+def divi(a, b):
+    return a/b
+
+a=int(input("Dame un numero:\n"))
+b=int(input("Dame otro numero:\n"))
+
+x=mult(a, b)
+print("La multiplicacion es: ", x)
+x=divi(a, b)
+print("La division es: ", x)
